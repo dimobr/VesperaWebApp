a4b0a93442b0ffdc5dce787e31afd52f84b4d3ab
dimobr <d96obradovic@gmail.com>
2023-11-20 14:32:06 +0100

    Add basic UI components for home page.

--- VesperaWebAppUI/src/app/app-routing.module.ts
+++ VesperaWebAppUI/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageContainerComponent } from './homepage/homepage-container/homepage-container.component';
+import { HeaderComponent } from './homepage/header/header.component';
+import { FooterComponent } from './homepage/footer/footer.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'main-content', component: HomepageContainerComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ VesperaWebAppUI/src/app/homepage/homepage.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HomepageContainerComponent } from './homepage-container/homepage-container.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HomepageContainerComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomepageModule { }
