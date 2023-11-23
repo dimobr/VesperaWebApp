8aa043c5e03587f7e15dc98c29c3661b2c988a9b
dimobr <d96obradovic@gmail.com>
2023-11-23 23:10:49 +0100

    Add constants, models, necessary services and form registration controller.

---
+++ VesperaWebApp/Services/FormRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿using VesperaWebApp.Models;
+
+namespace VesperaWebApp.Services
+{
+    public class FormRegistrationService : IFormRegistrationService
+    {
+        public void RegisterForm(RegistrationForm form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RegisterFormAsync(RegistrationForm form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
